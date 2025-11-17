88360ddf9929bc8d41b9dc2c7aae9e0250637027
Low-go <lorranalvesgaldino98@gmail.com>
2025-11-17 10:21:02 -1000

    on broken links warn

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,8 @@ const config: Config = {
   organizationName: 'Low-go', // Usually your GitHub org/user name.
   projectName: 'ISS-Documentation0Hub', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn', 
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
